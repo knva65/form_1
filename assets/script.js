b91255ef6287bbe38d7e7a1e26c8398c9ba2070d
knva65 <91244269+knva65@users.noreply.github.com>
2022-05-02 13:07:29 +1100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,21 @@
 
- function func() {
-var textname = document.getElementById("text_name_1").value;
-if(document.getElementById("text_name").value === '' || document.getElementById("text_name_1").value === '') {alert ("Заполните все поля!!"); }
-else {alert ('Добро пожаловать на сайт, ' + textname + ' !')};}
 
+let errors = [];
+function checkValidity(input) {
+    let validity = input.validity;
+    if(validity.valueMissing) { errors.push('Поле ' + input.placeholder + ' не заполнено'); }
+    if(validity.patternMismatch) { errors.push(input.placeholder + ' - неверный формат заполнения'); }
+    if(validity.rangeOverflow) { let max = input.max; errors.push('Максимальное значение не может быть больше чем ' + max); }
+    if(validity.rangeUnderflow) { let min = input.min; errors.push('Минимальное значение не может быть больше чем ' + min); }
+}
+
+function checkAll() {
+    errors = [];
+    let inputs = document.querySelectorAll("input");
+
+    for(let input of inputs) {
+        checkValidity(input);
+    }
+
+    document.getElementById('errorMessage').innerHTML=errors.join('. </br>');
+}
